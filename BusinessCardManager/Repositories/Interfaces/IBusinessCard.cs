ce8e132ef73f57dfed5de4942721bd0fb595228c
Muath Mhawich <23041194@student.ltuc.com>
2024-10-15 03:16:18 +0300

    Implement Repository Design Pattern and add Controllers and Services
    - Added Repository interfaces and implementations to manage data access and encapsulate business logic.
    - Included Controllers for handling incoming HTTP requests and returning appropriate responses.
    - Implemented Services for business logic and interaction between Controllers and Repository layer.
    
    This commit enhances the application's structure by following the Repository Design Pattern, promoting clean architecture and maintainability.

---
+++ BusinessCardManager/Repositories/Interfaces/IBusinessCard.cs
@@ -0,0 +1,18 @@
+﻿using BusinessCardManager.Models;
+using BusinessCardManager.Models.DTO;
+
+namespace BusinessCardManager.Repositories.Interfaces
+{
+    public interface IBusinessCard
+    {
+        Task<IEnumerable<BusinessCardResDto>> GetAllAsync(string name = null);
+        Task<BusinessCardResDto> GetByIdAsync(int id);
+        Task <string> AddBusinessCardAsync(BusinessCardReqDto card);
+        Task  DeleteAsync(int id);
+        Task UpdateAsync(int id,BusinessCardReqDto card);
+
+        Task<string> ExportToXmlAsync();
+        Task<string> ExportToCsvAsync();
+        Task<string> CreateDatabaseBackupAsync(string backupDirectory);
+    }
+}
---
+++ BusinessCardManager/data/BusinessCardDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BusinessCardManager.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessCardManager.data
+{
+    public class BusinessCardDbContext  : DbContext
+    {
+
+        
+            public BusinessCardDbContext(DbContextOptions<BusinessCardDbContext> options) : base(options)
+            {
+            }
+
+ 
+            public DbSet<BusinessCard> BusinessCards { get; set; }
+
+  
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<BusinessCard>(entity =>
+            {
+                entity.HasKey(e => e.Id); 
+                entity.Property(e => e.Id).ValueGeneratedOnAdd();
+                entity.Property(b => b.Name).IsRequired().HasMaxLength(1000);
+                entity.Property(b => b.Email).IsRequired().HasMaxLength(1000);
+                entity.Property(b => b.Gender).IsRequired().HasMaxLength(1000);
+                entity.Property(b => b.Address).IsRequired().HasMaxLength(1000);
+               
+            });
+        }
+        }
+}
